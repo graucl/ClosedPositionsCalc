32dccdf510f68a6daf425a57497b868742943798
DESKTOP-3TUS2RC\Lluis Grau <lluis.grau@col.vueling.com>
2021-07-29 22:31:32 +0200

    try catch implemented to controller

--- ClosedPositionsCalc.Distributed.WebServices/Controllers/FileController.cs
+++ ClosedPositionsCalc.Distributed.WebServices/Controllers/FileController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using System;
 using System.IO;
 using System.Threading.Tasks;
@@ -14,30 +15,55 @@ namespace ClosedPositionsCalc.Distributed.WebServices.Controllers
     {
         private readonly IConfiguration _config;
         private readonly IIncomeAppService _incomeAppService;
+        private readonly ILogger<FileController> _logger;
         private string _excelFile;
 
-        public FileController(IConfiguration config, IIncomeAppService incomeAppService)
+        public FileController(IConfiguration config, IIncomeAppService incomeAppService, ILogger<FileController> logger)
         {
             this._config = config;
             this._incomeAppService = incomeAppService;
+            this._logger = logger;
         }
 
         [HttpPost]
         public async Task<IActionResult> UploadFile(IFormFile file)
         {
-            _excelFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, _config.GetSection("FileSettings:Name").Value);
-            await SaveFile(file);
-            await _incomeAppService.Calculations(_excelFile);
+            try
+            {
+                _excelFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, _config.GetSection("FileSettings:Name").Value);
+                await SaveFile(file);
+                await _incomeAppService.Calculations(_excelFile);
 
-            return Ok();
+                return Ok();
+            }
+            catch (ArgumentNullException ex)
+            {
+                _logger.LogError("Error: " + ex.Message);
+            }
+            catch (FileNotFoundException ex)
+            {
+                _logger.LogError("Error: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error: " + ex.Message);
+            }
+
+            return Problem();
         }
 
         private async Task<bool> SaveFile(IFormFile file)
         {
             var success = false;
+            _excelFile = null;
 
             try
             {
+                if (_excelFile == null)
+                {
+                    throw new FileNotFoundException("File path not found");
+                }
+
                 var path = _excelFile;
 
                 using (var stream = new FileStream(path, FileMode.Create))
@@ -47,9 +73,9 @@ namespace ClosedPositionsCalc.Distributed.WebServices.Controllers
 
                 return success = true;
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-
+                _logger.LogError("Error: " + ex.Message);
             }
 
             return success;
